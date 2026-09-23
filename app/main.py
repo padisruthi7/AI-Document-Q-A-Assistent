6a8d0f1a7223a51a1322595fe93465c18c632374
Sruthi Padi <padisruthi7@gamil.com>
2026-09-23 18:44:31 +0530

    Update project

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ from app.services.gemini import GeminiService
 from app.services.vector_store import VectorStore
 
 app = FastAPI(title="AI Document Q&A Assistant", version="1.0.0")
-app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
+app.add_middleware(CORSMiddleware, allow_origins=["http://127.0.0.1:8000", "http://localhost:8000"], allow_methods=["GET", "POST"], allow_headers=["Content-Type"],)
 app.mount("/static", StaticFiles(directory="frontend"), name="static")
 
 store = VectorStore(CHROMA_DIR)
